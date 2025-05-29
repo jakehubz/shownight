cd6f059c447065759db1bc5e65794c1d0ec7c55d
jakehubz <jakeottosen@gmail.com>
2025-05-28 23:59:35 -0500

    0.0.0.0

--- app.py
+++ app.py
@@ -64,5 +64,8 @@ def sms_reply():
     resp.message(reply.strip())
     return str(resp)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
